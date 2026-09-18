4dbc3ba65f6553e04c682e0dc31e464fb9000809
chirckut-oss <chirckut@gmail.com>
2026-09-18 23:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
   image = Image.open(uploaded_file)
-  st.image(image, caption="Uploaded Image", use_column_width=True)
+  st.image(image, caption="Uploaded Image", use_container_width=True)
 
 if st.button("Translate to Hindi/English"):
   if not api_key:
